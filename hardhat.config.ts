ac9887d6a8c1f1fe521fe1b13c6371afa044d2d9
Seweryn Kras <sewerynkras@gmail.com>
2022-08-14 21:24:01 +0200

    gas report + tst coverage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.7",
+  gasReporter: {
+    enabled: true,
+  },
 };
 
 export default config;
